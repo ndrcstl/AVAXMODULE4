4cfbd8a5c09b234e4b518be7788a0731aa9ef1a5
ndrcstl <148858656+ndrcstl@users.noreply.github.com>
2024-07-20 11:45:41 +0800

    Update DegenToken.sol

--- DegenToken.sol
+++ DegenToken.sol
@@ -1,61 +1,69 @@
 // SPDX-License-Identifier: MIT
-pragma solidity ^0.8.9;
+pragma solidity ^0.8.18;
 
-import "@openzeppelin/contracts/token/ERC20/ERC20.sol";
-import "@openzeppelin/contracts/access/Ownable.sol";
-import "@openzeppelin/contracts/token/ERC20/extensions/ERC20Burnable.sol";
+import "@openzeppelin/contracts@4.9.0/token/ERC20/ERC20.sol";
+import "@openzeppelin/contracts@4.9.0/access/Ownable.sol";
 
-contract DegenToken is ERC20, Ownable, ERC20Burnable {
-    enum ItemType { TomeOfEXP, BagOfHealthPotions, BagOfManaPotions, ElixirOfInvulnerability }
-    mapping(ItemType => uint256) public itemPrices;
-    mapping(address => mapping(ItemType => uint256)) public inventory;
+contract DegenToken is ERC20, Ownable {
+    string[] private shop = [
+        "HealthPotion",
+        "ManaPotion",
+        "TomeOfExp",
+        "ElixirOfStrength",
+        "ElixirOfAgility",
+        "ElixirOfIntelligence",
+        "Antidote",
+        "Medicine",
+        "ElixirOfInvincibility",
+        "DevilsBlood"
+    ];
 
-    constructor() ERC20("Degen", "DGN") Ownable(msg.sender) {
-        itemPrices[ItemType.TomeOfEXP] = 50;
-        itemPrices[ItemType.BagOfHealthPotions] = 20;
-        itemPrices[ItemType.BagOfManaPotions] = 20;
-        itemPrices[ItemType.ElixirOfInvulnerability] = 100;
+    mapping(string => uint256) public shopPrices;
+    mapping(address => string) public inventory;
+
+    constructor() ERC20("Degen", "DGN") {
+        _mint(msg.sender, 1000000 * 1 ** uint(decimals()));
+
+        shopPrices["HealthPotion"] = 20 * 1 ** uint(decimals());
+        shopPrices["ManaPotion"] = 20 * 1 ** uint(decimals());
+        shopPrices["TomeOfExp"] = 50 * 1 ** uint(decimals());
+        shopPrices["ElixirOfStrength"] = 70 * 1 ** uint(decimals());
+        shopPrices["ElixirOfAgility"] = 70 * 1 ** uint(decimals());
+        shopPrices["ElixirOfIntelligence"] = 70 * 10 ** uint(decimals());
+        shopPrices["Antidote"] = 60 * 1 ** uint(decimals());
+        shopPrices["Medicine"] = 60 * 1 ** uint(decimals());
+        shopPrices["ElixirOfInvincibility"] = 120 * 1 ** uint(decimals());
+        shopPrices["DevilsBlood"] = 150 * 1 ** uint(decimals());
     }
 
-    function mint(address to, uint256 amount) public onlyOwner{
-        _mint(to, amount); 
+    function checkBalance(address account) public view returns (uint256) {
+        return balanceOf(account);
     }
 
-    function decimals() override public pure returns (uint8){
-        return 0;
+    function burn(uint256 amount) public {
+        _burn(_msgSender(), amount);
     }
 
-    function getBalance() public view returns (uint256){
-        return this.balanceOf(msg.sender);
+    function mint(address to, uint256 amount) public onlyOwner {
+        _mint(to, amount);
     }
 
-    function transferTokens(address _receiver, uint256 _value) public{
-        require(balanceOf(msg.sender)>= _value, "You do not have enough Degen Tokens");
-        _transfer(msg.sender, _receiver, _value);
+    function transfer(address to, uint256 amount) public virtual override returns (bool) {
+        _transfer(_msgSender(), to, amount);
+        return true;
     }
 
-    function burnTokens(uint256 _value) public{
-        require(balanceOf(msg.sender)>= _value, "You do not have enough Degen Tokens");      
-        _burn(msg.sender, _value);
+    function redeem(string memory itemName) external {
+        require(shopPrices[itemName] > 0, "Item does not exist");
+        uint256 shopPrice = shopPrices[itemName];
+        require(balanceOf(msg.sender) >= shopPrice, "Insufficient balance to buy this item!");
+
+        _burn(msg.sender, shopPrice);
+
+        inventory[msg.sender] = itemName;
     }
 
-    event RedeemedTokens(address indexed user, ItemType itemType, uint256 itemPrice);
-    event UpdatedInventory(address indexed user, ItemType itemType, uint256 newBalance);
-
-    function redeemTokens(ItemType _itemType) public {
-        uint256 itemPrice = itemPrices[_itemType];
-        require(balanceOf(msg.sender) >= itemPrice, "You do not have enough Degen Tokens");
-        inventory[msg.sender][_itemType]++;
-        emit RedeemedTokens(msg.sender, _itemType, itemPrice);
-        emit UpdatedInventory(msg.sender, _itemType, inventory[msg.sender][_itemType]);
-        _burn(msg.sender, itemPrice);
-    }
-
-    function getItemName(ItemType _itemType) internal pure returns (string memory) {
-        if (_itemType == ItemType.TomeOfEXP) return "Tome of EXP";
-        if (_itemType == ItemType.BagOfHealthPotions) return "Bag of Health Potions";
-        if (_itemType == ItemType.BagOfManaPotions) return "Bag of Mana Potions";
-        if (_itemType == ItemType.ElixirOfInvulnerability) return "Elixir of Invulnerability";
-        return "";
+    function getRedeemedItem(address account) external view returns (string memory) {
+        return inventory[account];
     }
 }
